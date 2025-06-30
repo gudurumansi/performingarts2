1f62321ae50039997a24d6b68d560dcc78c87cce
gudurumansi <152054159+gudurumansi@users.noreply.github.com>
2025-06-30 21:14:29 +0530

    Add files via upload

---
+++ Message.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const MessageSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ['general', 'question', 'resource', 'system'],
+        default: 'general'
+    },
+    image: {
+        type: String
+    },
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
